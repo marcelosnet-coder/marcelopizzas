8b6be2723bb0a6d33653010e8d4fc4eb29007189
marcelosnet-coder <marcelosnet@gmail.com>
2025-10-12 19:53:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const numeroWhatsApp = "55819953505957"; // seu número com DDD
+
+const pizzas = [
+  { nome: "Mussarela", preco: 25.0, ingredientes: "Mussarela, orégano", imagem: "https://i.imgur.com/6D8d0Mh.jpg" },
+  { nome: "Calabresa", preco: 30.0, ingredientes: "Calabresa, cebola, orégano", imagem: "https://i.imgur.com/fzvJv0G.jpg" },
+  { nome: "Frango com Catupiry", preco: 32.0, ingredientes: "Frango, catupiry, orégano", imagem: "https://i.imgur.com/LKXv8wH.jpg" },
+  { nome: "Mussarela com Cheddar", preco: 28.0, ingredientes: "Mussarela, cheddar, orégano", imagem: "https://i.imgur.com/4H40h8q.jpg" },
+  { nome: "Mussarela com Catupiry", preco: 28.0, ingredientes: "Mussarela, catupiry, orégano", imagem: "https://i.imgur.com/9vAqf5P.jpg" },
+  { nome: "Bacon", preco: 34.9, ingredientes: "Mussarela, bacon, orégano", imagem: "https://i.imgur.com/KB1pFZL.jpg" }
+];
